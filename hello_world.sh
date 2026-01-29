3f5cfba33028701bf42fa622620b1abb126ec3ed
kc-proton <kccollegeofengineering7@protonmail.com>
2026-01-29 14:22:30 +0000

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Hello World"
 curl -v -LOC - "https://sourceforge.net/projects/kccos/files/2025/kccos-2025.03.20-x86_64-BetaRelease.iso/download" &> new_file.txt
+rm download
 curl -v -LOC - "https://sourceforge.net/projects/kccos/files/2025/kccos-2025.07.10-x86_64-BetaRelease2.iso/download" &> new_file2.txt
